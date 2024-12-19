f14419fd2d3e772956b59fa082f61ed0e9ef5c65
kanisorn <qawqaw63123@gmail.com>
2024-12-19 09:18:19 +0700

    jwt setup

--- index.js
+++ index.js
@@ -6,19 +6,40 @@ const bodyParser = require('body-parser');
 app.use(cors());
 app.use(bodyParser.json());
 
+const jwt = require('jsonwebtoken');
+const SECRET = "your_secret_key";
+
 const port = 3000
 
-app.get('/login', (req, res) => {
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (!token) return res.status(401).json({ message: 'Access Denied' });
+
+    jwt.verify(token, SECRET, { expiresIn: '24h' },(err, user) => {
+        if (err) {
+            console.log("Invaild Token")
+            return res.status(403).json({message: 'Invalid Token'});
+        }
+        console.log("JWT PASSED")
+        req.user = user;
+        next();
+    });
+}
+
+app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
 app.post('/login', (req, res) => {
     username = req.body.username;
     password = req.body.password;
-
     console.log(username, password);
-
 })
+
+
+
 
 
 app.listen(port, () => {
